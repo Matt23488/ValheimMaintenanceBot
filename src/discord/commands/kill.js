c70771c49658497dd8a349c0caadea444b5800f0
Matthew Bryant <matt23488@gmail.com>
2021-04-08 17:22:45 +0000

    refactored kill command

--- src/discord/commands/kill.js
+++ src/discord/commands/kill.js
@@ -12,16 +12,13 @@ module.exports = {
      * @returns {Promise<void>}
      */
     execute: async function (message, rest) {
-        async function exit() {
-            await message.channel.send('bye');
-            message.client.destroy();
-        }
-
         if (valheimServer.isRunning()) {
             message.channel.send('Stopping server...');
             await valheimServer.stop();
             await message.channel.send('Server stopped.');
-            await exit();
-        } else await exit();
+        }
+
+        await message.channel.send('bye');
+        message.client.destroy();
     }
 };
