11c6c70a946ec448bf28501a863f2c377970876e
Matthew Bryant <matt23488@gmail.com>
2021-04-08 16:41:09 -0400

    git pull command

---
+++ src/discord/commands/git.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const roles = require('../roles');
+const { spawn } = require('child_process');
+const path = require('path');
+
+module.exports = {
+    role: roles.Admin,
+
+    /**
+     * 
+     * @param {Discord.Message} message 
+     * @param {string} rest 
+     * @returns {Promise<void>}
+     */
+    execute: async function (message, rest) {
+        return new Promise(resolve => {
+            if (rest === 'pull') {
+                let output = '';
+                const gitPull = spawn('git', [ rest ], { cwd: path.join(__dirname, '..') });
+                gitPull.stdout.on('data', data => {
+                    output = output + '\n' + data.toString();
+                });
+
+                gitPull.on('close', (code, signal) => {
+                    message.channel.send(`\`git pull\` output:\n\`\`\`${output}\`\`\``);
+                    resolve();
+                });
+
+            } else {
+                message.channel.send(`\`${rest}\` is an invalid \`git\` command, or I don't know how to do it.`);
+                resolve();
+            }
+        });
+    }
+};
