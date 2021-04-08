72765bec9a2ab7dd732436205a588e35e6f06d99
Matthew Bryant <matt23488@gmail.com>
2021-04-08 14:35:37 -0400

    invalidate command

---
+++ src/discord/commands/invalidate.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const config = require('../../config');
+const roles = require('../roles');
+
+module.exports = {
+    role: roles.Admin,
+
+    /**
+     * 
+     * @param {Discord.Message} message 
+     * @param {string} rest 
+     * @returns {Promise<void>}
+     */
+    execute: async function (message, rest) {
+        rest = rest.trim(); // TODO: remove this at some point when it's no longer necessary
+        try {
+            delete require.cache[require.resolve(`./commands/${rest}`)];
+        } catch (e) {
+            message.channel.send(`The ${config.discord.commandPrefix}${rest} command has not been used or does not exist.`);
+            return;
+        }
+
+        message.channel.send(`The ${config.discord.commandPrefix}${rest} command has been refreshed.`);
+    }
+};
