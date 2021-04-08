6d0432466c145ac3aad1dfec7a41acbc6129ed70
Matthew Bryant <matt23488@gmail.com>
2021-04-08 14:23:37 -0400

    testing new status command

--- src/discord/commands/status.js
+++ src/discord/commands/status.js
@@ -15,6 +15,18 @@ module.exports = {
      */
     execute: async function (message, rest) {
         if (!valheimServer.isRunning()) message.channel.send('The server is not currently started. Use `!start` to start the server.');
-        else message.channel.send(`The server is currently running at \`${getServerIpAddress()}:${config.valheim.port}\`.`);
+        // else message.channel.send(`The server is currently running at \`${getServerIpAddress()}:${config.valheim.port}\`.`);
+        else {
+            const embed = new Discord.MessageEmbed()
+                .setColor('#9900ff')
+                .setTitle(`${config.valheim.name} Server Status`)
+                .addField('Connected players', `${valheimServer.connectedPlayers.length} player${valheimServer.connectedPlayers.length === 1 ? '' : 's'} connected`);
+
+            valheimServer.connectedPlayers.forEach(p => embed.addField(p.name, '', true));
+
+            embed.setTimestamp();
+
+            message.channel.send(embed);
+        }
     }
 };
