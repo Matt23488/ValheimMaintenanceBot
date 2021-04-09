3172227a6c58ffa73c7ded01923e5436222cdffb
Matthew Bryant <matt23488@gmail.com>
2021-04-09 10:27:39 -0400

    Save changes

--- src/discord/commands/save.js
+++ src/discord/commands/save.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const config = require('../../config');
 const valheimServer = require('../../valheimServer');
 const roles = require('../roles');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
     role: roles.Admin,
@@ -14,7 +16,13 @@ module.exports = {
      */
     execute: function (message, rest) {
         return new Promise(resolve => {
-            if (rest === 'stdout') {
+            if (rest.length === 0) {
+                message.reply('save what, hoss?');
+                resolve();
+            }
+
+            const params = rest.split(' ');
+            if (params[0] === 'stdout') {
                 const prefix = '`stdout` output:\n```';
                 const suffix = '```';
                 const output = valheimServer.stdoutBuffer.toArray().join('\n');
@@ -23,12 +31,11 @@ module.exports = {
 
                 if (totalLength > 2000) final = prefix + '...' + output.substring(output.length - (2000 - prefix.length - suffix.length - 3)) + suffix;
                 else final = prefix + output + suffix;
-
-                //const final = prefix + output.substring(output.length - 2000 - prefix.length - suffix.length);
-
-                console.log(final.length);
                 message.channel.send(final);
-            } else message.channel.send(`\`${rest}\` is not a valid parameter of ${config.discord.commandPrefix}save.`);
+
+                if (params[1].length > 0) fs.writeFileSync(path.join(__dirname, `logs/${message.author.username}_${params[1]}.txt`), `${message.author.tag} saved \n\n${output}`);
+
+            } else message.channel.send(`\`${params[0]}\` is not a valid parameter of ${config.discord.commandPrefix}save.`);
         });
     }
 };
