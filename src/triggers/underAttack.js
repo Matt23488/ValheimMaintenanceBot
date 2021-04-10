c3a03c053a8c58d0a9e611840809ec9ae229bf9d
Matthew Bryant <matt23488@gmail.com>
2021-04-09 20:10:56 -0400

    Updated the underAttack trigger

--- src/triggers/underAttack.js
+++ src/triggers/underAttack.js
@@ -13,22 +13,65 @@ module.exports = {
     execute: function (text, searchTextIndex) {
         return new Promise(resolve => {
             const type = text.slice(searchTextIndex + this.searchText.length, text.indexOf('\n')).trim();
-            // Random event set:foresttrolls (detect these) (army_bonemass is another)
     
+            // https://valheim.fandom.com/wiki/Events
             let message = '';
+            let duration = 0;
             switch (type) {
-                case 'foresttrolls':
-                    message = 'The ground is shaking! Trolls are attacking!';
+                // Story events
+                case 'army_eikthyr':
+                    message = 'Eikthyr rallies the creatures of the forest! Boars and Necks are attacking!';
+                    duration = 90;
+                    break;
+                case 'army_theelder':
+                    message = 'The forest is moving... Greydwarfs, Greydwarf Brutes, and Greydwarf Shamans are attacking!';
+                    duration = 120;
                     break;
                 case 'army_bonemass':
-                    message = 'There\'s a foul smell from the Swamp! Draugr and Skeletons are attacking!';
+                    message = 'A foul smell from the swamp! Draugar and Skeletons are attacking!';
+                    duration = 150;
                     break;
+                case 'army_moder':
+                    message = 'A cold wind blows from the mountains! It\'s cold and Drakes are attacking!';
+                    duration = 150;
+                    break;
+                case 'army_goblin':
+                    message = 'The horde is attacking! Fulings, Fuling Berserkers, and Fuling Shamans! AHHHHHHHHHHHHHHHHHHH!';
+                    duration = 120;
+                    break;
+
+                // Enemy events
+                case 'skeletons':
+                    message = 'Skeleton surprise! Skeletons are attacking!';
+                    duration = 120;
+                    break;
+                case 'blobs':
+                    message = 'Blobs are attacking!';
+                    duration = 120;
+                    break;
+                case 'foresttrolls':
+                    message = 'The ground is shaking! Trolls are attacking!';
+                    duration = 80;
+                    break;
+                case 'wolves':
+                    message = 'You are being hunted! Wolves are attacking!';
+                    duration = 120;
+                    break;
+                case 'surtlings':
+                    message = 'There\'s a smell of sulfur in the air! Surtlings are attacking!';
+                    duration = 120;
+                    break;
+
                 default:
                     message = `We're under attack! Dad needs to account for this type: \`${type}\`!`;
                     break;
             }
 
-            getClient().channels.cache.get(config.defaultChannel).send(message);
+            const channel = getClient().channels.cache.get(config.defaultChannel);
+            channel.send(message);
+
+            setTimeout(() => channel.send('The attack _should_ be over. Thank gods.'), duration * 1000);
+
             resolve();
         });
     }
