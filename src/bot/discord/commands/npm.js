41d4f26b25f2aa657627b09fa0520d1e36ab9e4e
Matthew Bryant <matt23488@gmail.com>
2021-04-11 17:11:24 -0400

    npm command

---
+++ src/bot/discord/commands/npm.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const roles = require('../roles');
+const { spawn } = require('child_process');
+const path = require('path');
+
+module.exports = {
+    name: 'npm',
+    description: 'Execute npm commands.',
+    role: roles.Admin,
+
+    /**
+     * 
+     * @param {Discord.Message} message 
+     * @param {string} rest 
+     * @returns {Promise<void>}
+     */
+    execute: async function (message, rest) {
+        return new Promise(resolve => {
+            let output = '';
+            const npm = spawn('npm', [ rest ], { cwd: path.join(__dirname, '../../../..') });
+            npm.stdout.on('data', data => {
+                output = output + '\n' + data.toString();
+            });
+
+            npm.on('close', (code, signal) => {
+                message.channel.send(`\`npm ${rest}\` output:\n\`\`\`${output}\`\`\``);
+                resolve();
+            });
+        });
+    }
+};
