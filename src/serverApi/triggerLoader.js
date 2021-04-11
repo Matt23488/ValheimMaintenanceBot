f21cfd07d9566cd03869c7a314b2957bacda07d0
Matthew Bryant <matt23488@gmail.com>
2021-04-11 10:25:45 -0400

    nothing really

--- src/serverApi/triggerLoader.js
+++ src/serverApi/triggerLoader.js
@@ -17,17 +17,5 @@ module.exports = {
 
             trigger.execute(parsed.data);
         }
-        // return new Promise(resolve =>{
-        //     const triggers = fs.readdirSync(path.join(__dirname, 'triggers')).map(f => require(path.join(__dirname, 'triggers', f)));
-
-        //     for (let trigger of triggers) {
-        //         const index = output.indexOf(trigger.searchText);
-        //         if (index === -1) continue;
-
-        //         trigger.execute(output, index);
-        //     }
-
-        //     resolve();
-        // });
     }
 };
