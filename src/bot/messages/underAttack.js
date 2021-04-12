962e696e9bfb20aba3bd74b58377b4b0f0944a96
Matthew Bryant <matt23488@gmail.com>
2021-04-12 11:55:56 -0400

    army_moder embed image

--- src/bot/messages/underAttack.js
+++ src/bot/messages/underAttack.js
@@ -16,6 +16,10 @@ module.exports =  {
             .setDescription(data.message);
 
         switch (data.type) {
+            case 'army_moder':
+                embed.setImage('https://i.pinimg.com/originals/73/56/55/73565582e9ab3974c9e2d54e063cb9a8.gif');
+                break;
+
             case 'skeletons':
                 embed.setImage('https://i.pinimg.com/originals/7f/e2/49/7fe2495ce378b955d79567ce2f5d5fb3.gif');
                 break;
