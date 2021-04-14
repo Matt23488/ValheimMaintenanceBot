f127238066212b705c0e297098d60682fdb2f980
Matthew Bryant <matt23488@gmail.com>
2021-04-14 16:27:33 -0400

    Took out the "attack is over" part of the underAttack trigger. It's not accurate if everyone leaves the red circle as that pauses the actual timer, but there's no way to know if that has happened from the server stdout.

--- src/bot/messages/underAttack.js
+++ src/bot/messages/underAttack.js
@@ -17,7 +17,7 @@ module.exports =  {
         const attack = config.valheim.attacks.find(a => a.name === data);
         if (attack) {
             embed.setDescription(attack.message).setImage(attack.image);
-            setTimeout(() => channel.send('The attack is over. Thank gods.'), attack.duration * 1000);
+            //setTimeout(() => channel.send('The attack is over. Thank gods.'), attack.duration * 1000);
         }
         else embed.setDescription(`Not sure what they are, but the server event is \`${data}\`!`);
 
