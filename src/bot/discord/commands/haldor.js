386e180bd1dd8e8e082a795972dc7e68cd174e2f
Matthew Bryant <matt23488@gmail.com>
2021-04-14 00:33:52 +0000

    fixed haldor command, sort of.

--- src/bot/discord/commands/haldor.js
+++ src/bot/discord/commands/haldor.js
@@ -1,8 +1,4 @@
 const Discord = require('discord.js');
-const fs = require('fs');
-const path = require('path');
-const config = require('../../../config');
-const BinaryFile = require('binary-file');
 
 module.exports = {
     name: 'haldor',
@@ -16,34 +12,25 @@ module.exports = {
      * @returns {Promise<void>}
      */
     execute: async function (message, rest) {
-        //await message.reply('pong');
-        //const world = fs.readFileSync(path.join(config.valheim.worldDirectory, `${config.valheim.world}.db`));
-        const world = new BinaryFile(path.join(config.valheim.worldDirectory, `${config.valheim.world}.db`), 'r');
-        await world.open();
-        const bytes = await world.read(world.size());
-        await world.close();
-
-        // 56 65 6E 64 6F 72 5F 42 6C 61 63 6B 46 6F 72 65 73 74
-        /**
-         * @type {{ x: number, y: number, z: number }[]}
-         */
-        const locations = [];
-        const searchString = 'Vendor_BlackForest';
-        while (true) {
-            const index = bytes.indexOf(searchString);
-            if (index === -1) break;
-
-            locations.push({
-                x: bytes.readFloatLE(index + searchString.length + 0),
-                y: bytes.readFloatLE(index + searchString.length + 4),
-                z: bytes.readFloatLE(index + searchString.length + 8)
-            });
-        }
+        // TODO: Parse the world file. Tried it once but it was really slow so I manually parsed
+        // the values and hardcoded them for now. Obviously this only works for our world.
+        const locations = [
+            { x: 7814.27685546875, y: 46.8652153015137, z: 3194.57763671875 },
+            { x: -7404.109375, y: 32.0077476501465, z: 3761.87646484375 },
+            { x: -519.125366210937, y: 71.7126235961914, z: -6257.04931640625 },
+            { x: -9708.294921875, y: 42.8488845825195, z: 643.530090332031 },
+            { x: -8846.37109375, y: 37.3945922851562, z: -3340.88012695313 },
+            { x: -687.266479492188, y: 45.0766296386719, z: -2446.80712890625 },
+            { x: 9405.5185546875, y: 66.6875, z: 182.668472290039 },
+            { x: 7481.71923828125, y: 31.1889266967773, z: -3257.50952148438 },
+            { x: -1744.00402832031, y: 47.6506233215332, z: -1290.71606445312 },
+            { x: -8714.86328125, y: 40.4701538085937, z: -270.191589355469 },
+        ];
 
         message.channel.send(new Discord.MessageEmbed()
             .setColor(0x0099ff)
             .setTitle('Haldor Locations')
-            .addFields(locations.map(l => { return { name: '\u200B', value: `(${l.x}, ${l.y}, ${l.z})`, inline: true }; }))
+            .addFields(locations.map(l => { return { name: '\u200B', value: `\`(${l.x}, ${l.y}, ${l.z})\``, inline: true }; }))
         );
     }
 };
