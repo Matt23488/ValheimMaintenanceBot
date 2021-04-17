5de942c89d3649b43133a55bbc19e6bb15d750f2
Matthew Bryant <matt23488@gmail.com>
2021-04-17 10:59:39 -0400

    speech is coming soon

--- src/bot/discord/commandManager.js
+++ src/bot/discord/commandManager.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const config = require('../../config');
-const fs = require('fs');
 
 /**
  * Executes a bot command.
--- src/bot/messages/echo.js
+++ src/bot/messages/echo.js
@@ -1,5 +1,9 @@
+const Discord = require('discord.js');
 const config = require('../../config');
 const discordBot = require('../discord/bot');
+const say = require('say');
+const { set } = require('lodash');
+const fs = require('fs');
 
 module.exports =  {
     /**
@@ -8,5 +12,30 @@ module.exports =  {
      */
     execute: function (data) {
         discordBot.getClient().channels.cache.get(config.defaultChannel).send(data);
+        // say.speak(data, 'Microsoft Zira Desktop');
+        // say.getInstalledVoices(console.log);
+
+        /**
+         * @type {Discord.VoiceChannel}
+         */
+        const voice = discordBot.getClient().channels.cache.get(config.defaultVoiceChannel);
+        voice.join().then(connection => {
+            say.export(data, null, null, 'test.wav', err => {
+                const dispatcher = connection.play('test.wav');
+
+                dispatcher.on('start', () => {
+                    console.log(`saying '${data}'`);
+                });
+
+                dispatcher.on('finish', () => {
+                    console.log('done');
+                    connection.disconnect();
+                    fs.unlinkSync('test.wav');
+                });
+
+                dispatcher.on('error', console.error);
+
+            });
+        });
     }
 };
